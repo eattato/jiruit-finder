c1a4f8193b6d701c0e89ac4b8105464041d2cf77
eattato <80892781+eattato@users.noreply.github.com>
2022-04-18 11:45:33 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ String.prototype.format = function () {
   return result;
 };
 
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 $(document).bind("contextmenu", function () {
   return false;
 });
@@ -29,10 +33,16 @@ $(document).ready(function () {
   resize();
   $(window).resize(resize);
 
+  var timerId = -1;
   $(".apply").click(function () {
+    if (timerId != -1) {
+      clearInterval(timerId);
+    }
+    $(".timer").text("0");
     // generates boxes
     boxSize = Number($(".grid").val());
     bombCounts = Number($(".bombcount").val());
+    console.log(bombCounts);
     $(".button").remove();
     for (var count = 1; count <= boxSize * boxSize; count++) {
       $(".frame").append("<div class='button'></div>");
@@ -52,10 +62,17 @@ $(document).ready(function () {
 
           if ($(".opened").length == 0) {
             // generate map
+            var timer = 0;
+            timerId = setInterval(function () {
+              timer += 1;
+              $(".timer").text(timer);
+            }, 1000);
+
             bombPlaces = [];
             boxes = [];
+
+            // get this pos
             for (var ind = 1; ind <= boxSize; ind++) {
-              var row = [];
               for (var ind2 = 1; ind2 <= boxSize; ind2++) {
                 if (
                   $(".button")
@@ -63,6 +80,20 @@ $(document).ready(function () {
                     .hasClass("opening")
                 ) {
                   thisPos = (ind - 1) * boxSize + ind2 - 1;
+                }
+              }
+            }
+
+            var safeLength = Math.floor(
+              Math.sqrt(boxSize * boxSize - bombCounts)
+            );
+
+            for (var ind = 1; ind <= boxSize; ind++) {
+              var row = [];
+              for (var ind2 = 1; ind2 <= boxSize; ind2++) {
+                currentPos = (ind - 1) * boxSize + ind2 - 1;
+                if ($(".button").eq(currentPos).hasClass("opening")) {
+                  thisPos = currentPos;
                   row.push(1);
                 } else {
                   row.push(0);
@@ -71,15 +102,82 @@ $(document).ready(function () {
               }
               boxes.push(row);
             }
+
+            // console.log("bomb:", bombPlaces.length);
+            var safeDirection = 1;
+            var safePos = thisPos;
+            var safeStack = 1;
+            // console.log("safe zone", safeLength);
+            while (safeLength > 0) {
+              // wraps clicked position and makes safe zone near spawn
+              for (var stack = 1; stack <= 2; stack++) {
+                if (safeLength > 0) {
+                  for (var count = 1; count <= safeStack; count++) {
+                    if (safeLength > 0) {
+                      if (safeDirection == 1) {
+                        // right
+                        safePos += 1;
+                      } else if (safeDirection == 2) {
+                        // down
+                        safePos += boxSize;
+                      } else if (safeDirection == 3) {
+                        // left
+                        safePos -= 1;
+                      } else if (safeDirection == 4) {
+                        // down
+                        safePos -= boxSize;
+                      }
+                      var col = Math.floor(safePos / boxSize);
+                      var row = safePos - col * (boxSize - 1);
+
+                      if (
+                        row < boxSize &&
+                        row >= 0 &&
+                        col < boxSize &&
+                        col >= 0
+                      ) {
+                        bombPlaces.splice(bombPlaces.indexOf(safePos), 1);
+                        safeLength -= 1;
+                        $(".button").eq(safePos).addClass("wrap");
+                      }
+                    } else {
+                      break;
+                    }
+                  }
+                  safeDirection += 1;
+                  if (safeDirection > 4) {
+                    safeDirection = 1;
+                  }
+                } else {
+                  break;
+                }
+              }
+              safeStack += 1;
+            }
+
+            // console.log("clicked:", thisPos);
+            // console.log("safezone left:", safeLength);
+            console.log("bomb placeable:", bombPlaces.length);
+            // console.log("place gogogo");
+            // console.log("placeable bombs:", bombCounts);
+            console.log(bombPlaces);
+            var placedBomb = 0;
             for (var ind = 1; ind <= bombCounts; ind++) {
-              var bombPlace = Math.floor(Math.random() * bombPlaces.length) + 1;
+              placedBomb += 1;
+              var bombPlace = rand(0, bombPlaces.length - 1);
+              // var bombPlace = Math.floor(Math.random() * bombPlaces.length);
+              // console.log(bombPlace, bombPlaces[bombPlace], bombPlaces);
+              // console.log("placing:", bombPlaces[bombPlace]);
               $(".button").eq(bombPlaces[bombPlace]).addClass("bomb");
               boxes[Math.floor(bombPlaces[bombPlace] / boxSize)][
                 bombPlaces[bombPlace] -
                   Math.floor(bombPlaces[bombPlace] / boxSize) * boxSize
               ] = 2;
-              bombPlaces.splice(bombPlaces.indexOf(bombPlace), 1);
+              var before = bombPlaces.length;
+              bombPlaces.splice(bombPlace, 1);
+              console.log(bombPlaces.length - before);
             }
+            // console.log("placed all bomb", placedBomb);
           }
 
           for (var ind = 1; ind <= boxSize; ind++) {
@@ -170,14 +268,37 @@ $(document).ready(function () {
                   .append("<div class='opened'></div>");
               }
             } else {
-              $(".button")
-                .eq(col * boxSize + row)
-                .addClass("active");
+              $(".button").addClass("active");
+              if (timerId != -1) {
+                clearInterval(timerId);
+              }
             }
           }
 
           open(Math.floor(thisPos / boxSize), thisPos % boxSize);
           $(this).removeClass("opening");
+          var clear = true;
+          for (var col = 1; col <= boxSize; col++) {
+            for (var row = 1; row <= boxSize; row++) {
+              if (boxes[ind - 1][row - 1] == 0) {
+                console.log("block not opened");
+                clear = false;
+                break;
+              } else if (
+                boxes[col - 1][row - 1] == 2 &&
+                $(".button")
+                  .eq(ind - 1)
+                  .find(".flag").length == 0
+              ) {
+                console.log("bomb not flagged");
+                clear = false;
+                break;
+              }
+            }
+          }
+          if (clear == true) {
+            window.alert("클리어!");
+          }
         }
       } else {
         // flag box
@@ -187,6 +308,28 @@ $(document).ready(function () {
         } else {
           // remove flag
           $(this).find(".flag").remove();
+        }
+        var clear = true;
+        for (var col = 1; col <= boxSize; col++) {
+          for (var row = 1; row <= boxSize; row++) {
+            if (boxes[ind - 1][row - 1] == 0) {
+              console.log("block not opened");
+              clear = false;
+              break;
+            } else if (
+              boxes[col - 1][row - 1] == 2 &&
+              $(".button")
+                .eq(ind - 1)
+                .find(".flag").length == 0
+            ) {
+              console.log("bomb not flagged");
+              clear = false;
+              break;
+            }
+          }
+        }
+        if (clear == true) {
+          window.alert("클리어!");
         }
       }
     });
